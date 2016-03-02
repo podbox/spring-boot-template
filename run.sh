489c655bcef2a0dedcc06e9bca079c4d2909dd6d
Guillaume Livron <guillaume@podbox.com>
2016-03-02 10:21:27 +0100

    update dependencies

--- run.sh
+++ run.sh
@@ -6,5 +6,4 @@ set -e
 ./gradlew clean build
 docker build -t podbox/spring-boot-template src/docker/
 
-BUILD_LIBS=`pwd`/build/libs/
-docker run -v $BUILD_LIBS:/libs/ -e ENV=prod --name podbox-template -t podbox/spring-boot-template
+docker run -v `pwd`/build/libs/:/libs/ -e ENV=prod --name podbox-template -t podbox/spring-boot-template
